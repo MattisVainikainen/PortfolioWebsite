5349647d3c5e3173c4bf2c5a70772ceb11f06e7f
mattisvainikainen <mattis.vainikainen@gmail.com>
2021-02-17 15:37:57 +0100

    try nr 5555

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <Navigation />
         <Switch>
           <Route path="/" exact component={Start} />

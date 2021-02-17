d280652f23bf48e2efac6c7215b31ef710806fb5
mattisvainikainen <mattis.vainikainen@gmail.com>
2021-02-17 15:58:40 +0100

    rrr

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Router basename={"/PortfolioWebsite/"}>
+      <Router basename={"/PortfolioWebsite"}>
         <Navigation />
         <Switch>
           <Route path="/" exact component={Start} />
